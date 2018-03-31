4af1b364fa1350778b2b1e97179e7387415e9295
Carlos Saldarriaga <c.saldarriaga@javeriana.edu.co>
2018-03-31 13:58:56 -0500

    Sección 12 terminada - backend terminado

--- config/config.js
+++ config/config.js
@@ -1,1 +1,6 @@
 module.exports.SEED = 'este-es-un-seed-dificil';
+
+
+// Google
+module.exports.GOOGLE_CLIENT_ID = '407653393606-gkqcgie06vnp6onvas02557i73jqlb77.apps.googleusercontent.com';
+module.exports.GOOGLE_SECRET = '419yGmuFsHotn-Hi-z1sslDG';
